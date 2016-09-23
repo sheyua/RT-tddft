ffd6308fa2f789412d2604d39b50181908abf1a5
Yuan Shen <sy0302@sherlock-ln03.stanford.edu>
2016-09-23 02:58:19 -0700

    change tddft_compute report format

--- rt-tddft/src/tddft_compute.f90
+++ rt-tddft/src/tddft_compute.f90
@@ -9,7 +9,8 @@ SUBROUTINE tddft_compute(istep)
   USE mp_global,    ONLY : intra_pool_comm
   USE mp,           ONLY : mp_sum
   USE io_global,    ONLY : stdout
-  USE tddft_module, ONLY : rpos, num_elec, dipole
+  USE extfield,     ONLY : evolt
+  USE tddft_module, ONLY : rpos, num_elec, dipole, dt
   implicit none
   integer, intent(in) :: istep
   integer :: is, ipol
@@ -35,10 +36,77 @@ SUBROUTINE tddft_compute(istep)
   call mp_sum(dipole, intra_pool_comm)
 #endif
 
-    ! print observables
-    write(stdout, *) 'CHARGE', 1, istep, num_elec(1)
-    write(stdout,'(''DIP    '',I1,1X,I6,3E16.6)') 1, istep, dipole(:,1)
+  ! print legend
+  if(istep == 0) then
+    write(stdout,'(5X,A4,1X,A6,1X,A16,1X,A16,1X,A16)') 'Spin', 'istep', 'Vbias', 'Charge', 'dipole' 
+  endif
+
+  ! print observables
+  do is = 1, nspin
+    write(stdout,'(5X,I4,1X,I6,1X,ES16.8,1X,ES16.8,1X,3ES16.8)') is, istep, evolt, num_elec(is), dipole(:,is)
+  enddo
+
+  call flush_unit( stdout )
 
   call stop_clock('tddft_compute')
 END SUBROUTINE tddft_compute
 !---
+
+
+!    !========================================================================
+!    ! output rho%of_r() and v%of_r()
+!    !========================================================================
+!    if (istep == 1) then
+!      index0 = 0
+!#ifdef __PARA
+!      write(filename, '(A8,I0)') "rho_",  me_pool
+!        do idz = 1, me_pool
+!          index0 = index0 + dfftp%npp(idz)
+!        enddo
+!#else
+!      write(filename, '(A8)') "rho"
+!#endif
+!      fzcur = 32
+!      open(unit=fzcur,file=trim(filename))
+!      write(fzcur, *) "#Procid=", me_pool, nspin, dfftp%nnr, dfftp%nr1x, dfftp%nr2x
+!    endif ! open charge density writer
+!
+!    if (istep == 1) then
+!#ifdef __PARA
+!      write(filename, '(A8,I0)') "v_",  me_pool
+!#else
+!      write(filename, '(A8)') "v_"
+!#endif
+!      fzfield = 33
+!      open(unit=fzfield,file=trim(filename))
+!      write(fzfield, *) "#Procid=", me_pool, nspin, dfftp%nnr, dfftp%nr1x, dfftp%nr2x
+!    endif ! open zKS potential writer
+!
+!    write(fzcur,*) "TimeStep: ", istep
+!    write(fzfield,*) "TimeStep: ", istep
+!    idz = 0
+!    id = 0
+!    do
+!      if (id >= dfftp%nnr ) exit
+!
+!      tmprho = 0.d0
+!      tmpv = 0.d0
+!      do idy = 1, dfftp%nr2x
+!      do idx = 1, dfftp%nr1x
+!        do is = 1, nspin
+!          tmprho(is) = tmprho(is) + rho%of_r( id+1, is )
+!          tmpv(is) = tmpv(is) + v%of_r( id+1, is )
+!        enddo
+!        id = id + 1
+!      enddo
+!      enddo
+!
+!      do is = 1, nspin
+!        write(fzcur,*) index0+idz, is, tmprho(is)*volRatio
+!        write(fzfield,*) index0+idz, is, tmpv(is)/(dfftp%nr2x * dfftp%nr1x)
+!      enddo
+!      idz = idz + 1
+!    enddo
+!    !========================================================================
+!    ! output rho%of_r() and v%of_r()
+!    !========================================================================
